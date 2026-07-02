6778d5fae44c70720b30dccbfc6390ada2e42f55
praveenkelim <kpraveen9059@gmail.com>
2026-07-02 12:49:24 +0530

    removed old commands from script

--- script.sh
+++ script.sh
@@ -31,19 +31,4 @@ echo "Git commit:"
 git log --oneline -1
 
 echo "===== Build Completed Successfully ====="#!/bin/bash
-set -x
-set -e
-
-echo "this is first practice project"
-
-echo "current directory"
-
-pwd
-
-whoami
-
-df -h
-
-free -h
-
 
